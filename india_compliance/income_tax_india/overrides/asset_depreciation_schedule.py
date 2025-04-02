e4c2c3c9cfbeed07c423b2dc0d9504dac5f3d672
Khushi Rawat <142375893+khushi8112@users.noreply.github.com>
2025-04-02 15:22:23 +0530

    chore: update imports

--- india_compliance/income_tax_india/overrides/asset_depreciation_schedule.py
+++ india_compliance/income_tax_india/overrides/asset_depreciation_schedule.py
@@ -12,39 +12,18 @@ from frappe.utils import (
     month_diff,
 )
 from erpnext.accounts.utils import get_fiscal_year
-from erpnext.assets.doctype.asset_depreciation_schedule.asset_depreciation_schedule import (
-    get_asset_depr_schedule_doc,
-    get_default_wdv_or_dd_depr_amount,
-)
+from erpnext.assets.doctype.asset_depreciation_schedule.asset_depreciation_schedule import get_asset_depr_schedule_doc
+from erpnext.assets.doctype.asset_depreciation_schedule.depreciation_methods import WDVMethod
 
 
-def get_wdv_or_dd_depr_amount(
-    asset,
-    fb_row,
-    depreciable_value,
-    yearly_opening_wdv,
-    schedule_idx,
-    prev_depreciation_amount,
-    has_wdv_or_dd_non_yearly_pro_rata,
-    asset_depr_schedule,
-    prev_per_day_depr=0,
-):
+def get_wdv_or_dd_depr_amount(asset_depreciation_schedule, row_idx):
 
     # As per IT act, if the asset is purchased in the 2nd half of fiscal year, then rate is divided by 2 for the first year
 
     if not fb_row.finance_book or not frappe.db.get_value(
         "Finance Book", fb_row.finance_book, "for_income_tax"
     ):
-        return get_default_wdv_or_dd_depr_amount(
-            asset,
-            fb_row,
-            depreciable_value,
-            schedule_idx,
-            prev_depreciation_amount,
-            has_wdv_or_dd_non_yearly_pro_rata,
-            asset_depr_schedule,
-            prev_per_day_depr=prev_per_day_depr,
-        )
+        return WDVMethod.calculate_wdv_or_dd_based_depreciation_amount(asset_depreciation_schedule, row_idx)
 
     asset_depr_schedule.flags.wdv_it_act_applied = True
 
